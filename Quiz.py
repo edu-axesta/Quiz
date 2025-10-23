e21b6a523761265542d71d958ebdbafead1bb3d5
Axel Ståhle <AxelS@5e604d9b56f1>
2025-10-23 12:38:08 +0000

    Del 1: En fråga med svarsalternativ

---
+++ Quiz.py
@@ -0,0 +1,10 @@
+print("Vilken huvudstad har sverige?\n1. Oslo\n2. Stockholm\n3. Köpenhamn")
+
+stad=int(input(""))
+
+if stad==1 or stad==1:
+    print(f"Ditt svar: {stad} \nFel!")
+elif stad==3:
+    print(f"Ditt svar: {stad} \nFel!")
+elif stad ==2:
+    print(f"Ditt svar: {stad} \nRätt")
